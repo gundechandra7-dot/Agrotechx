a6aef5ea65a1246d339bc2bd208026a2a7f88ba2
gundechandra7-dot <gundechandra7@gmail.com>
2026-03-17 09:05:39 +0530

    Update API endpoint to production URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://agrotechx-backend.onrender.com";
 
 export const detectDisease = async (file) => {
   const formData = new FormData();
